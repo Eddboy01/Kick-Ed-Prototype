5649fa72e9045ce054d14306bbbd5896637a312c
Eddboy01 <jacksonedward29@gmail.com>
2019-10-29 21:30:37 -0500

    Added Initial files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const sideNav = document.getElementById("side-navigation");
+
+function openNav() {
+  console.log("sauce");
+  sideNav.classList.add("active");
+}
+
+function closeNav() {
+  console.log("sauce");
+  sideNav.classList.remove("active");
+}
